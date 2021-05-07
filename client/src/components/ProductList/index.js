8273d983ec262444f1616a7d6f16660d95149946
unknown <km_si@ymail.com>
2021-05-07 14:53:19 -0400

    implement react-redux in Productitem index

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import ProductItem from "../ProductItem";
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
